2810c460f03f01097f8f156711a0f095f6fb52b9
Emelie Hallenberg <emeliehallenberg@gmail.com>
2023-06-13 14:23:50 +0200

    add Notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>Planera Din Sommar</h1>
+    </div>
+  )
+}
+
+
+function Main() {
+  const [plan, setPlan] = useState("");
+  const [month, setMonth] = useState("");
+  const [planList, setPlanList] = useState([]);
+
+  const handleChange = (event) => {
+    const newPlan = event.target.value;
+    setPlan(newPlan);
+  }
+
+  const handleChangeMonth = (event) => {
+    const month = event.target.value;
+    setMonth(month);
+  }
+
+  const handleClick = () => {
+   setPlanList([...planList, {month: month, plan: plan}])
+  }
+
+  return (
+    <div className="input">
+      <input type="text" placeholder="Lägg till plan..." onChange={handleChange}/>
+    <br/><label>Välj månad: </label><select onChange={handleChangeMonth}>
+ <option value=''>---</option>
+      <option value='Juni'>Juni</option>
+    <option value='Juli'>Juli</option>
+    <option value='Augusti'>Augusti</option></select>
+    <br/><button onClick={handleClick}>Lägg till</button>
+    <div className="display">
+      <Display list={planList}/>
+    </div>
+    </div>
+  )
+}
+
+
+function Display({list}) {
+const displayList = list.map(el => {
+return <li>{el.month}: <span>{el.plan}</span><br/><Notes/></li>
+}) 
+return (
+<div>
+  <h2>Mina planer:</h2>
+  <ul>{displayList}</ul>
+</div>
+)
+}
+
+function Notes() {
+
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Header/>
       </header>
+      <div className="Main">
+      <Main/>
+      </div>
     </div>
   );
 }
