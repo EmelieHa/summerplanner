d97da4a98212183e4a7bdb38bc52cc42d8ccb87e
Emelie Hallenberg <emeliehallenberg@gmail.com>
2023-06-15 08:11:47 +0200

    add picture and edit text

--- src/App.js
+++ src/App.js
@@ -1,79 +1,97 @@
-import {React, useState} from 'react';
-import './App.css';
+import { React, useState } from 'react'
+import './App.css'
+import beach from './beach.jpg'
 
-function Header() {
+function Header () {
   return (
     <div>
       <h1>Planera Din Sommar</h1>
+      <img alt='strand' src={beach} />
     </div>
   )
 }
 
+function Main () {
+  const [plan, setPlan] = useState('')
+  const [month, setMonth] = useState('')
+  const [planList, setPlanList] = useState([])
 
-function Main() {
-  const [plan, setPlan] = useState("");
-  const [month, setMonth] = useState("");
-  const [planList, setPlanList] = useState([]);
-
-  const handleChange = (event) => {
-    const newPlan = event.target.value;
-    setPlan(newPlan);
+  const handleChange = event => {
+    const newPlan = event.target.value
+    setPlan(newPlan)
   }
 
-  const handleChangeMonth = (event) => {
-    const month = event.target.value;
-    setMonth(month);
+  const handleChangeMonth = event => {
+    const month = event.target.value
+    setMonth(month)
   }
 
   const handleClick = () => {
-   setPlanList([...planList, {month: month, plan: plan}])
+    setPlanList([...planList, { month: month, plan: plan }])
   }
 
   return (
-    <div className="input">
-      <input type="text" placeholder="Lägg till plan..." onChange={handleChange}/>
-    <br/><label>Välj månad: </label><select onChange={handleChangeMonth}>
- <option value=''>---</option>
-      <option value='Juni'>Juni</option>
-    <option value='Juli'>Juli</option>
-    <option value='Augusti'>Augusti</option></select>
-    <br/><button onClick={handleClick}>Lägg till</button>
-    <div className="display">
-      <Display list={planList}/>
-    </div>
+    <div className='input'>
+      <input
+        type='text'
+        placeholder='Lägg till plan...'
+        onChange={handleChange}
+      />
+      <br />
+      <label>Välj månad: </label>
+      <select onChange={handleChangeMonth}>
+        <option value=''>---</option>
+        <option value='Juni'>Juni</option>
+        <option value='Juli'>Juli</option>
+        <option value='Augusti'>Augusti</option>
+      </select>
+      <br />
+      <button onClick={handleClick}>Lägg till</button>
+
+      <div className='display'>
+        <Display list={planList} />
+      </div>
     </div>
   )
 }
 
-
-function Display({list}) {
-const displayList = list.map(el => {
-return <li>{el.month}: <span>{el.plan}</span><br/><Notes/></li>
-}) 
-return (
-<div>
-  <h2>Mina planer:</h2>
-  <ul>{displayList}</ul>
-</div>
-)
-}
-
-function Notes() {
-
-}
-
-
-function App() {
+function Display ({ list }) {
+  const displayList = list.map(el => {
+    return (
+      <li>
+        {el.month}: <span>{el.plan}</span>
+        <br />
+        <Notes />
+      </li>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-       <Header/>
+    <div>
+      <h2>Mina planer:</h2>
+      <ul>{displayList}</ul>
+    </div>
+  )
+}
+
+function Notes () {
+  return (
+    <div className='notes'>
+<p id='edit'>Redigera</p>
+    </div>
+  )
+}
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Header />
       </header>
-      <div className="Main">
-      <Main/>
+      <div className='Main'>
+        <Main />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
